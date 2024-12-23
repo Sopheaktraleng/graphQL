5afb9500e6b4fdf7915545fd21415f7267af7962
Leng Sopheaktra <sopheaktraleng4@gmail.com>
2024-12-23 16:34:56 +0700

    feat: enable to query mutiple data

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ const resolvers = {
             return db.authors.find((author) => author.id === args.id);
         },
     },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id);
+        },
+    },
+    Review: {
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id);
+        },
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id);
+        },
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id);
+        },
+    },
 };
 // Set up Server
 const server = new ApolloServer({ typeDefs, resolvers });
